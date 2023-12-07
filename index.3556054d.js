cb0b155d9bef60a259f69a6c6ea2fa04743ad21f
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-07 17:47:56 +0100

    make build

---
+++ index.3556054d.js
@@ -0,0 +1,3 @@
+function e(e,n){return new Promise(function(t){var c=function(o){t("It was ".concat(n," on the element: \n      ").concat(e.nodeName,", id: ").concat(e.id,".")),e.removeEventListener(n,c)};e.addEventListener(n,c)})}var n=function(e){var n=document.createElement("div");n.className="message",n.innerText=e,document.body.append(n)},t=document.getElementById("login"),c=document.getElementById("password"),o=document.getElementById("submit");e(t,"click").then(n),e(c,"click").then(n),e(o,"click").then(n),e(t,"input").then(n),e(c,"input").then(n),e(t,"blur").then(n),e(c,"blur").then(n),e(o,"blur").then(n);//# sourceMappingURL=index.3556054d.js.map
+
+//# sourceMappingURL=index.3556054d.js.map
